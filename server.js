78a5fd144eca7868693d3b33295df2ec7012e4c1
Jirka Kremser <jkremser@redhat.com>
2014-12-14 00:40:40 +0100

    fds

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 app.listen(port, ip, function(){
     console.log('%s: Node server started on %s:%d ...',
-        Date(Date.now() ), self.ipaddress, self.port);
+        Date(Date.now() ), ip, port);
     }
 );
 
